93426ddf985bfdd9bcd65d4ee3180d68f0e6c041
steven <steven@Hermes.local>
2017-07-22 10:07:16 -0400

    Add steven laptop to local config

--- local_config.py
+++ local_config.py
@@ -4,3 +4,6 @@ JOEY_PICKLE_PATH = '/scratch/PhysioNet/data.pkl'
 
 STEVEN_PATH = '/mnt/datadrive/projects/physionet/data/training2017/'
 STEVEN_PICKLE_PATH = '/mnt/datadrive/projects/physionet/data/data.pkl'
+
+STEVEN_LAPTOP_PATH = '/Users/steven/Documents/projects/ekg/data/training2017/'
+STEVEN_LAPTOP_PICKLE_PATH = '/Users/steven/Documents/projects/ekg/data/training2017/data.pkl'
